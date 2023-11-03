90e16b06bc60fbfcaae2bdaf804ca176925218b6
Eason Ong <eong@lcs.on.ca>
2023-11-03 09:15:33 -0400

    Initial Commit

---
+++ stopWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stopWatch
+//
+//  Created by Ong Eason on 3/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stopWatch/stopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopWatchApp.swift
+//  stopWatch
+//
+//  Created by Ong Eason on 3/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct stopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
