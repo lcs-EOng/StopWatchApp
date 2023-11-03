c19198c1ffee75fb8e3a53318974d62fbffb2d30
Eason Ong <eong@lcs.on.ca>
2023-11-03 09:34:29 -0400

    finished basic strucure of first button

--- stopWatch/ContentView.swift
+++ stopWatch/ContentView.swift
@@ -11,16 +11,32 @@ struct ContentView: View {
     var body: some View {
         ZStack{
             //First Layer (background)
-            Color.yellow
+            Color.black
                 .ignoresSafeArea()
             
             
             //Second layer (rest of interface)
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+                Text("00:09.96")
+                    .font(Font.system(size: 90))
+                    .fontWeight(.thin)
+                    .foregroundStyle(.white)
+                ZStack{
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 100)
+                    
+                    Circle()
+                        .foregroundColor(.black)
+                        .frame(width: 93)
+                    
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 89)
+                    Text("Reset")
+                        .foregroundStyle(.white)
+                        .font(.title2)
+                }
             }
             .padding()
         }
