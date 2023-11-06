467bb61aaf3c735bda676d5f881a2b186d9ed206
Eason Ong <eong@lcs.on.ca>
2023-11-05 22:28:22 -0500

    Added stored properties to CircleButtonView so we can use it to show multiple buttons. We do this by providing different arguments for the parameters when an instance of CircleButtonView is created.

---
+++ stopWatch/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  stopWatch
+//
+//  Created by Ong Eason on 3/11/2023.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
